39efe102fd1db8d01d95e0f0e6cbbe1f54487501
Sunrise <sunrisewng@gmail.com>
2019-08-03 18:23:56 +0200

    glitch shader

---
+++ Assets/Scripts/InstantiateGlitchGrabber.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateGlitchGrabber : MonoBehaviour
+{
+    private Material glitchMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        glitchMaterial = GetComponent<MeshRenderer>().material;
+        glitchMaterial.SetFloat("seed", Random.value * 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
