7314451edcf758acd3536625d865ae0174f1fed4
Ross Borchers <ross@renderheads.com>
2019-08-03 15:04:12 +0200

    sa

---
+++ Assets/ContentLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContentLog : MonoBehaviour
+{
+    internal void Add(GameManager.ContentPiece modifiedPiece)
+    {
+       
+    }
+
+    private void Update()
+    {
+
+    }
+}
