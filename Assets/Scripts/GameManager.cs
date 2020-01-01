8a6171dd5073745e41a40dda587b45a1937e25f6
Ross Borchers <ross@renderheads.com>
2020-01-01 03:40:11 +0200

    comitting changes after jam

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -95,7 +95,7 @@ public class GameManager : MonoBehaviour
                 {
                     GlitchObjects[i].SetActive(true);
                 }
-               
+                
             }
         }
     }
